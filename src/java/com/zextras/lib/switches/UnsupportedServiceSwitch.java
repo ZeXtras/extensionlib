cc081a77eb6b6878a417a318f82c8c4ac705a73e
Davide Baldo <db@zextras.com>
2017-04-26 16:24:07 +0200

    added UnsupportedServiceSwitch to create 'mock' services #2

--- src/java/com/zextras/lib/switches/UnsupportedServiceSwitch.java
+++ src/java/com/zextras/lib/switches/UnsupportedServiceSwitch.java
@@ -1,5 +1,46 @@
 package com.zextras.lib.switches;
 
-public class UnsupportedServiceSwitch
+public class UnsupportedServiceSwitch implements ServiceSwitch
 {
+  private final String mName;
+
+  public UnsupportedServiceSwitch(String name)
+  {
+    mName = name;
+  }
+
+  @Override
+  public void turnOn(SwitchConditionNotification conditionNotification) throws Service.ServiceStartException
+  {
+    throw new Service.ServiceStartException("Unsupported service");
+  }
+
+  @Override
+  public void turnOff()
+  {
+  }
+
+  @Override
+  public boolean isOn()
+  {
+    return false;
+  }
+
+  @Override
+  public boolean couldTurnOn(SwitchConditionNotification conditionNotification)
+  {
+    return false;
+  }
+
+  @Override
+  public boolean couldTurnOff()
+  {
+    return false;
+  }
+
+  @Override
+  public String name()
+  {
+    return mName;
+  }
 }
