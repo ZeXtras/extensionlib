de372a7b50366c344d376fe8dca7c54d3222dcdb
Matteo Battilana <matteo.battilana@zextras.com>
2019-04-12 15:07:40 +0200

    [DRIV-103] - Implemented DbHandler in MariaDbHandler

--- src/java/com/zextras/modules/chat/server/db/MariaDbHandler.java
+++ src/java/com/zextras/modules/chat/server/db/MariaDbHandler.java
@@ -27,7 +27,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 @Singleton
-public abstract class MariaDbHandler
+public abstract class MariaDbHandler implements DbHandler
 {
   private final ZimbraConnectionProvider mZimbraConnectionProvider;
 
